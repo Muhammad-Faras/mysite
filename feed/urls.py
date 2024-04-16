86d82ca4cb7bcd26791aeff180c76e1e954d1cc1
Muhammad Faras <noorfaras809@gmail.com>
2024-04-16 10:20:59 -0700

    small commit

--- feed/urls.py
+++ feed/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import feed_view
+from .views import feed_view, search_users_view
 
 app_name = 'feed'
 urlpatterns = [
-    path('', feed_view, name='feed')
+    path('', feed_view, name='feed'),
+    path('users/', search_users_view, name='search-users')
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
 # Create your views here.
 from posts.models import Post
 
@@ -12,4 +15,19 @@ def feed_view(request):
     return render(request, 'feed/feed.html', context)
 
 
+@login_required(login_url='/accounts/login/')
+def search_users_view(request):
+    context = {}
+    if request.method == 'POST':
+        search_query = request.POST.get('searchedquery')
+        searched_users = user.objects.filter(username__icontains=search_query) | user.objects.filter(email__icontains=search_query)
+        context['search_query'] = search_query
+        context['searched_users'] = searched_users
+    else:
+        result = "invalid"
+        context['result'] = result    
+
+    
+    return render(request, 'feed/search_users.html', context)
+    
 
