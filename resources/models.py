ac3dc8dc8da95448296a5ef801d4f2c745be41c9
Muhammad Faras <noorfaras809@gmail.com>
2024-05-19 14:11:17 +0500

    videllink model added in resources app

--- resources/models.py
+++ resources/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from accounts.models import Skill
 # Create your models here.
 class Books(models.Model):
     book_title = models.CharField(max_length=255)
@@ -7,4 +7,12 @@ class Books(models.Model):
     book_img = models.ImageField(upload_to='admin_books_images/')
     bookpdf = models.FileField(upload_to='admin_books/')
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class VideosLinks(models.Model):
+    skill = models.ForeignKey(Skill, on_delete=models.DO_NOTHING)
+    link_description = models.TextField()
+    link = models.URLField()
+    link_created_at = models.DateTimeField(auto_now_add=True)
+    
     
--- resources/views.py
+++ resources/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,HttpResponse
-from .models import Books
+from .models import Books,VideosLinks
 # Create your views here.
 
 
@@ -7,4 +7,8 @@ def books_view(request):
     context = {}
     books = Books.objects.all()
     context['books'] = books
+    
+    
+    links = VideosLinks.objects.all()
+    context['links'] = links
     return render(request, 'resources/books.html', context)
